c4b4ceb2d0eaa7162347c21f1e98f79ed93f07ae
Miguel Machado <miiguellmp@gmail.com>
2019-10-05 18:59:41 -0300

    dotenv url database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://mmcoding:gPV2iIWxYtc8vhf0@cluster0-j6dm1.mongodb.net/omniweek?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
